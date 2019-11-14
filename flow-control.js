2d97ea830d3c0a3c5ab80b1b5cdf4afd3c02615b
enid svymbersky <enid.bersky@gmail.com>
2019-11-14 18:11:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -34,19 +34,38 @@ function ternaryTeenager(age) {
 }
 ternaryTeenager(20);
 
-function switchAge(age) {
-  switch(age){
-    case age > 12 && age < 20:
-      console.log ("You are a teenager");
-      break;
-    default:
-    return ("You have an age");
-    
+  function switchAge(age) {
+    switch(age){
+      case 13:
+        return("You are a teenager");
+        
+      case 14:
+        return("You are a teenager");
+        break;
+      case 15:
+        return("You are a teenager");
+        break;
+      case 16:
+        return("You are a teenager");
+        break;
+      case 17:
+        return("You are a teenager");
+        break;
+      case 18:
+        return("You are a teenager");
+        break;
+      case 19:
+        return("You are a teenager");
+        break;
+      default:
+      return("You have an age");
+        
+        
+    }
+  
   }
-
-}
-
-switchAge(99);
+  
+  switchAge(13); 
 
 
 
