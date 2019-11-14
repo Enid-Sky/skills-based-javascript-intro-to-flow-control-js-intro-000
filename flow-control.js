a54ae0cb54fa0dd09552b8bc1512cb47a8d30083
enid svymbersky <enid.bersky@gmail.com>
2019-11-14 17:28:49 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,50 @@
 function basicTeenager(age) {
-
+  if (age > 12 && age < 20){
+  return("You are a teenager!");
 }
+}
+
+basicTeenager(13);
 
 function teenager(age) {
-
+  if (age > 12 && age < 20){
+  return("You are a teenager!");
+  }else {
+  return("You are not a teenager");
 }
+}
+
+teenager(20);
 
 function ageChecker(age) {
-
+if (age > 12 && age < 19) {
+  return("You are a teenager!");
+} else if (age < 13) {
+  return ("You are a kid");
+} else {
+  return("You are a grownup");
 }
+}
+ageChecker(25);
 
 function ternaryTeenager(age) {
+  return (age > 12 && age < 20)? "You are a teenager" : 
+  "You are not a teenager";
 
 }
+ternaryTeenager(20);
 
 function switchAge(age) {
+  switch(age){
+    case age 
+
+      break;
+    default:
+    console.log("You have an age");
+      
+      
+  }
 
 }
+
+
